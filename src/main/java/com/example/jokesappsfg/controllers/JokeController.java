0c766d84b8e8ed2f81d9010de7cdbcbf5bbe5576
metalogy <metalogy@wp.pl>
2022-04-09 00:23:41 +0200

    Chuck Norris Joke App

---
+++ src/main/java/com/example/jokesappsfg/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package com.example.jokesappsfg.controllers;
+
+import com.example.jokesappsfg.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/jokesappsfg/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.example.jokesappsfg.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
