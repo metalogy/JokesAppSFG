c00bd261424f98f6a073afe4f1dd6172ce89312c
metalogy <metalogy@wp.pl>
2022-04-08 23:51:10 +0200

    Initial commit

---
+++ src/main/java/com/example/jokesappsfg/JokesAppSfgApplication.java
@@ -0,0 +1,13 @@
+package com.example.jokesappsfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppSfgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesAppSfgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jokesappsfg/JokesAppSfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokesappsfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppSfgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
